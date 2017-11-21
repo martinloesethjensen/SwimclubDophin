3ad1eef4f3987c7e186295d87a6b21c1a5ddb702
ortismal <33696981+ortismal@users.noreply.github.com>
2017-11-21 12:39:03 +0100

    updates

--- SWC/Traener.java
+++ SWC/Traener.java
@@ -6,8 +6,18 @@ public class Traener extends Ansat {
 	public void subMenu() {
 		Scanner console = new Scanner(System.in);
       
-      
-      
+         
+         int menu = -1;
+            while (menu != 0) {
+               traenerMenu();
+               this = console.nextInt();
+               
+            switch(menu){
+               case 1:
+               case 2:
+               case 3:
+            }
+            
       
 	}
 
