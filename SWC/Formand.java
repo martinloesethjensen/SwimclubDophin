3677c736378e9997b31f2310a7270213b86685e7
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2017-11-21 12:45:06 +0100

    Change to formand

--- SWC/Formand.java
+++ SWC/Formand.java
@@ -1,17 +1,29 @@
 import java.util.*;
+import java.io.*;
 
 public class Formand extends Ansat {
+   public Formand(String firstName, String lastName, int alder, String aktivitetsForm){
+      super(firstName, lastName, alder, aktivitetsForm);
+   }
+   
+   public Formand(){
+   }
+   
+   public void saveMember(String fileName)throws Exception{
+      PrintStream toFile = new PrintStream(new File(fileName));
+      toFile.print(info());  
+   }
+   public String info(){
+      return firstName + " " + lastName + " " + alder + " " + aktivitetsForm;
+   }
 
 	/**
 	 * 
 	 * @param title
 	 */
-	public void subMenu() {
-		//switch her
+	public void subMenu(String title) {
+		
 	}
 
-	public void opretMedlem() {
-		//at oprette et medlem
-	}
 
 }
