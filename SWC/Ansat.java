fcaab1de2c2d8ea7bfea3f04944affab7d32d885
Valhalla <31356774+Martyfloorball@users.noreply.github.com>
2017-11-21 10:36:33 +0100

    update on SWC

--- SWC/Ansat.java
+++ SWC/Ansat.java
@@ -1,16 +1,33 @@
+import java.util.*;
+
 public class Ansat {
 
 	private String firstName;
 	private String lastName;
 	private String title;
 
-	public void subMenu() {
+	public int subMenu() {
 		Scanner console = new Scanner(System.in);
-      //initialize objects
+      //initialize objekter
       Traener traenerLogin = new Traener();
       Kasserer kassererLogin = new Kasserer();
       Formand formandLogin = new Formand();
+      
+      int menu = -1;
+      while (menu != 0){
+         
+         menu = console.nextInt(); 
+      }
+      
+      
+      return 0;
 	}
+   public void menuInfo(){
+      System.out.println("Tast 1 for formand");
+      System.out.println("Tast 2 for kasserer");
+      System.out.println("Tast 3 for træner");
+      System.out.println("Tast 0 for at komme tilbage");
+   }
 
 	public String getFirstName() {
 		return this.firstName;
--- SWC/Traener.java
+++ SWC/Traener.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Traener extends Ansat {
 
 	/**
