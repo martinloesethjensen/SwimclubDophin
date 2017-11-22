04a5b111bafa8624c52e4aede1487e35160f26c1
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2017-11-22 14:24:39 +0100

    Temp. update på "Formand"

--- SWC/Formand.java
+++ SWC/Formand.java
@@ -10,7 +10,8 @@ import java.io.*;
 public class Formand extends Ansat {
    
    public Formand(){}
-   
+   Scanner console = new Scanner(System.in);
+
    /*public Formand(String firstName, String lastName, int alder, String aktivitetsForm){
       super(firstName, lastName, alder, aktivitetsForm);
    }*/
@@ -22,7 +23,6 @@ public class Formand extends Ansat {
 	*/
    
 	public void subMenu() {
-	   Scanner console = new Scanner(System.in);
       int menu = -1;
       while(menu != 0){
          subMenuInfo();
@@ -50,18 +50,29 @@ public class Formand extends Ansat {
       System.out.println("Tast 1 for at oprette nyt medlem");
       System.out.print("Tast 0 for at gaa tilbage\n<==");
    }
-   
+   //Metod der samler den skrevne info i en string
+   public String info(){
+      return firstName + " " + lastName + " " + alder + " " + aktivitetsForm;
+   }
+   //Metode til at gemme til fil
+   public void saveIt(String filNavn)throws Exception{
+      PrintStream newMemberInfo = new PrintStream(new File(filNavn));
+      newMemberInfo.print(info() + "/n");
+         
+   }
    /**
    *
    * Opretter og gemmer nyt medlem i en fil
    */
-   public void opretMedlem(String fileName)throws Exception{
+   public void opretMedlem(Scanner console)throws Exception{
+      System.out.println("Indtast fornavn");
+      firstName = console.next();
+      System.out.println("Indtast efternavn");
+      lastName = console.next();
+      System.out.println("Indtast alder");
+      alder = console.nextInt();
+      System.out.println("Vælg aktivitetsform /nTast 1 for Motionist/nTast 2 for Konkurrence"); 
       //få input om medlems info her
-      PrintStream toFile = new PrintStream(new File(fileName));
-      //toFile.print(info());  
+       
    }
-   /*public String info(){
-      return firstName + " " + lastName + " " + alder + " " + aktivitetsForm;
-   }*/
-
 }
