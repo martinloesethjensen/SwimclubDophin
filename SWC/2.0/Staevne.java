60da5de7892dfed9bbf0084ad315789478b55478
Valhalla <31356774+Martyfloorball@users.noreply.github.com>
2017-11-24 13:28:49 +0100

    Update 2.0

---
+++ SWC/2.0/Staevne.java
@@ -0,0 +1,9 @@
+import.java.util.*;
+import java.io.*;
+
+public class Staevne extends KonkurrenceSvoemmer {
+   
+   public Staevne (String fornavn, String efternavn, String staevneNavn, String disciplin, int staevneTid, int placering){
+      super(fornavn, efternavn, staevneNavn, disciplin, staevneTid, placering);
+   }
+}
