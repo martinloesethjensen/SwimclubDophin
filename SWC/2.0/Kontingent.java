27b020026ef270dc16a0d8f79f5b6f38a6e46511
ortismal <33696981+ortismal@users.noreply.github.com>
2017-11-24 13:43:53 +0100

    hejhej

---
+++ SWC/2.0/Kontingent.java
@@ -0,0 +1,22 @@
+import.java.util.*;
+import.java.io.*;
+
+public class Kontingent {
+
+   private int alder;
+   private double pris;
+   
+   public Kontingent(){}
+      
+   public Kontingent (int alder) {}
+   
+   public Kontingent (int alder, double pris) {}
+   
+   
+}
+
+public int getAlder() {return this.alder;}
+public int setAlder(alder) {this.alder = alder;}
+
+public double getPris() {return this.pris;}
+public double setPris(pris) {this.pris = pris;}
