c00cf34aa2c598d04e491753fd8b6cd288a083b1
Valhalla <31356774+Martyfloorball@users.noreply.github.com>
2017-11-24 13:43:59 +0100

    update hope

---
+++ SWC/2.0/Passiv.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Passiv extends Kontigent  {
+   
+   public Passiv(double pris){
+      super(pris);
+   }
+   //getter and setters til pris 
+   
+   
+   //polymorphisme
+   public double getPris() { return super.getPris() + 500.0; } 
+}
