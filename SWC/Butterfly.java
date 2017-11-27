e83cddaed634d4e1431fa1abeb6abf5d4b98768d
Valhalla <31356774+Martyfloorball@users.noreply.github.com>
2017-11-27 15:42:42 +0100

    Tilføjelser til discipliner

--- SWC/Butterfly.java
+++ SWC/Butterfly.java
@@ -1,8 +1,23 @@
-import.java.util.*;
-import.java.io.*;
+import java.util.*;
+import java.io.*;
 
-public class Butterfly extends Disciplin {
+public class Butterfly extends Medlem {
 
-
-
+   public Butterfly(String fornavn, String efternavn, String staevneNavn, double staevneTid, int placering, String disciplin)throws Exception{
+      super.setFornavn(fornavn);
+      super.setEfternavn(efternavn);
+      super.setStaevneNavn(staevneNavn);
+      super.setStaevneTid(staevneTid);
+      super.setPlacering(placering);
+      super.setDisciplin(disciplin);
+      
+      //gemmer til fil
+      super.saveIt("butterfly.txt");
+   }
+   
+   @Override   
+   public String toString(){
+      return super.getFornavn()+" "+super.getEfternavn()+" "+super.getStaevneNavn()+" "
+               +super.getDisciplin()+" "+super.getStaevneTid()+" "+super.getPlacering();
+   }
 }
--- SWC/Hundesvømning.java
+++ SWC/Hundesvømning.java
@@ -1,9 +1,23 @@
 import.java.util.*;
 import.java.io.*;
 
-public class Hundesvømning extends Disciplin {
+public class Hundesvømning extends Medlem {
 
-//statements
-
-
+   public Crawl(String fornavn, String efternavn, String staevneNavn, double staevneTid, int placering, String disciplin)throws Exception{
+      super.setFornavn(fornavn);
+      super.setEfternavn(efternavn);
+      super.setStaevneNavn(staevneNavn);
+      super.setStaevneTid(staevneTid);
+      super.setPlacering(placering);
+      super.setDisciplin(disciplin);
+      
+      //gemmer til fil
+      super.saveIt("hundesvoemning.txt");
+   }
+   
+   @Override   
+   public String toString(){
+      return super.getFornavn()+" "+super.getEfternavn()+" "+super.getStaevneNavn()+" "
+               +super.getDisciplin()+" "+super.getStaevneTid()+" "+super.getPlacering();
+   }
 }
