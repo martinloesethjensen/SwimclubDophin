eec62e672dac656bf5c1f019cce34f3164f95daa
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2017-11-27 10:57:54 +0100

    WIP - traener

--- SWC/Traener.java
+++ SWC/Traener.java
@@ -24,6 +24,7 @@ public class Traener extends Ansat {
             case 1: printDisciplin(); break; //tilføj switch
             case 2: printKandidater(); break; //tilføj switch
             case 3: printListe(); break; //printe fra konkurrencesvømmere fil
+            case 4: registrerStaevne(console); break; //tilføjer navn + stævne til fil
             case 0: 
                this.menu = -1; //for at den ikke også hopper ud af ansats menu
                super.menu(); //menu hos ansat
@@ -41,6 +42,7 @@ public class Traener extends Ansat {
       System.out.println("Tast 1 for disciplinliste med tilknyttede medlemmer");
       System.out.println("Tast 2 for top 5 liste over disciplin"); //ændres
       System.out.println("Tast 3 for liste over konkurrencesvoemmere");
+      System.out.println("Tast 4 for at registrere deltagelse i stævne");
       System.out.println("Tast 0 for at gaa tilbage\n<=="); 
    }
    
@@ -70,6 +72,24 @@ public class Traener extends Ansat {
       System.out.println("<==|_Kom_ind_|==>");
       transferToList("konkurrenceSvoemmere.txt");
       subMenu();
+   }
+   
+   //Beskrivelse
+   
+   public void registrerStaevne(Scanner console)throws Exception{
+      System.out.println("Enter-check");
+      System.out.println("Indtast fornavn");
+      super.setFornavn(console.next());
+      System.out.println("Indtast efternavn");
+      super.setEfternavn(console.next());
+      System.out.println("Indtast stævnenavn");
+      super.setStaevneNavn(console.next());
+      
+      System.out.println("Vælg disciplin");
+      Formand refMetode = new Formand();
+      refMetode.vaelgDisciplin(console);
+      System.out.println(refMetode.getDisciplin()); 
+      
    }
    
    /**
