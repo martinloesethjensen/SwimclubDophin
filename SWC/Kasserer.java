6d592b5593832c065906016c3bd41169ada540e8
Valhalla <31356774+Martyfloorball@users.noreply.github.com>
2017-11-30 21:28:55 +0100

    redigerKontingent lavet færdig og den er komplet
    Ved ikke om den følger UC

--- SWC/Kasserer.java
+++ SWC/Kasserer.java
@@ -4,6 +4,8 @@ import java.io.*; // for PrintStream
 
 public class Kasserer extends Ansat {
    
+   protected int counter = 0; //brugt i redigerKontingent
+   
    public Kasserer(){}
    
    public Kasserer(String fornavn, String efternavn, String restance){
@@ -39,7 +41,7 @@ public class Kasserer extends Ansat {
                super.menu(); //menu hos ansat
                break;
             default: 
-               System.out.println("Tast venligst et nummer der er fremvist\n");
+               System.out.println("TAST VENLIGST ET NUMMER DER ER FREMVIST\n");
                subMenu();
          }
          break; //for kun at komme ud af dette loop
@@ -47,8 +49,8 @@ public class Kasserer extends Ansat {
 	}
    
    public void subMenuInfo(){
-      System.out.printf("%-25s%s\n%-25s%s\n%-25s%s\n", "PRINT RESTANCELISTE", 
-            "TRYK 1", "PRINT BETALINGSSTATUS", "TRYK 2", "RETURNER", "TAST 0");
+      System.out.printf("%-25s%s\n%-25s%s\n%-25s%s\n%-25s%s\n", "PRINT RESTANCELISTE", 
+            "TRYK 1", "PRINT BETALINGSSTATUS", "TRYK 2", "REDIGER BETALING","TAST 3", "RETURNER", "TAST 0");
    }
 
    
@@ -119,7 +121,75 @@ public class Kasserer extends Ansat {
       return " ";
    }
    
-   public void redigerKontigent(Scanner console){
-      System.out.println("Hej konti");
+   public int count()throws Exception{
+      Scanner scanFil = new Scanner(new File("visRestance.txt"));  
+      //Printf så det ser pænt og formateret ud 
+      while(scanFil.hasNextLine()){
+         this.counter++;
+         System.out.println(this.counter + " " + scanFil.nextLine());
+      }
+      return this.counter;
+   }
+   
+   public void redigerKontigent(Scanner console)throws Exception{
+      Scanner scanRest = new Scanner(new File("visRestance.txt")); 
+      count();
+      String redigerRestance[][] = new String[this.counter][3];
+      
+      while(scanRest.hasNext()){
+         for(int i = 0; i < this.counter; i++){
+            for(int j = 0; j < 3; j++){
+               String item = scanRest.next();
+               redigerRestance[i][j] = item;
+            }
+         }
+      }
+      int dummy = 0;
+      while(dummy == 0){
+         System.out.println("\nTAST NUMMER PAA MEDLEM DU OENSKER AT OPDATERE BETALING PAA");
+         int num = console.nextInt(); //Check vaerdi?
+         
+         System.out.println("HAR MEDLEMMET BETALT TAST 1\nMANGLER MEDLEMMET AT BETALE TAST 2");
+         String input = console.next();
+         if(input.equals("1")){
+            setRestance("Betalt");
+            redigerRestance[num-1][2] = getRestance();
+            dummy = 1;
+         }else if(input.equals("2")){
+            setRestance("Restance");
+            redigerRestance[num-1][2] = getRestance();
+            dummy = 1; 
+         }else{ 
+            System.out.println("UGYLDIGT INPUT\nTAST VENLIGST ET TAL DER ER FREMVIST"); 
+            this.counter = 0;
+            redigerKontigent(console);
+         }    
+      }
+      
+      //Overskriver plads i eksisterende fil
+      PrintStream addChange = new PrintStream(new File("visRestance.txt"));
+      for(int i = 0; i < this.counter; i++){
+         addChange.println(redigerRestance[i][0] + " " + redigerRestance[i][1] + " " 
+            + redigerRestance[i][2]);
+      }
+      //clean up
+      scanRest.close();
+      this.counter = 0;
+      
+      System.out.println("...MEDLEMMETS BETALING ER NU BLEVET OPDATERET...\n");
+      int dummy2 = 0;
+      while(dummy2 == 0){
+         System.out.println("FOR AT OPDATERE ENDNU ET MEDLEMS BETALING TAST 1\nRETUNER TAST 0");
+         String fort = console.next();
+         if(fort.equals("1")){
+            redigerKontigent(console);
+            dummy2 = 1;
+         }else if(fort.equals("0")){
+            subMenu();
+            dummy2 = 1;
+         }else{  
+            System.out.println("UGYLDIGT INPUT\nTAST VENLIGST ET TAL DER ER FREMVIST"); 
+         }
+      }
    }
 }
