2a54fb86c7b53cca0c3e6374c7080ff3d2043315
ortismal <33696981+ortismal@users.noreply.github.com>
2017-12-01 13:33:15 +0100

    Fields til kontingent

--- SWC/Kontingent.java
+++ SWC/Kontingent.java
@@ -1,7 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-public class Kontingent extends Medlem {
+public class Kontingent {
+
+   protected String fornavn;
+   protected String efternavn;
+   protected int alder;
+   protected String medlemskab;
+   protected String disciplin;
+   protected double tid;
+   protected String dato;
    
    /*public Kontingent(){}
       
@@ -9,4 +17,27 @@ public class Kontingent extends Medlem {
    
    public Kontingent (int alder, double pris) { super(alder, pris); }
    */
+   
+   public String getFornavn() {return this.fornavn;}
+   public void setFornavn(String fornavn) {this.fornavn = fornavn;}
+
+   public String getEfternavn() {return this.efternavn;}
+   public void setEfternavn(String efternavn) {this.efternavn = efternavn;}
+
+   public int getAlder() {return this.alder;}
+   public void setAlder(int alder) {this.alder = alder;}
+   
+   public String getMedlemskab() {return this.medlemskab;}
+   public void setMedlemskab(String medlemskab) {this.medlemskab = medlemskab;}
+   
+   public String getDisciplin() {return this.disciplin;}
+   public void setDisciplin(String disciplin) {this.disciplin = disciplin;}
+
+   public double getTid() {return this.tid;}
+   public void getTid(double tid) {this.tid = tid;}
+   
+   public String getDato() {return this.dato;}
+   public void setDato(String dato) {this.dato = dato;}
 }
+
+
